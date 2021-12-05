6a1fcbf51b7bc0ab24dc7e680c50a23341cdeabf
SpyroLab <hardwoork22@gmail.com>
2021-12-05 17:18:12 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,368 @@
+<!DOCTYPE html>
+<html lang="uk">
+	<head>
+		<meta charset="UTF-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<title>Ремонт стиральных машин в киеве</title>
+		<link rel="shortcut icon" href="img/icon.png" />
+		<link rel="stylesheet" href="main.css" />
+		<link rel="stylesheet" href="fonts.css" />
+		<link rel="stylesheet" href="animate.css" />
+		<title>Document</title>
+	</head>
+	<body>
+		<header id="top">
+			<div class="container">
+				<div class="top-line">
+					<div class="wrapper-tagline">
+						<img
+							class="logo wow swing animate__swing"
+							src="img/logo.png"
+							alt="Logo"
+						/>
+						<div class="tagline">
+							<h2>Фирменный ремонт стиральных машин в</h2>
+							<span class="kyiv">Киеве</span>
+							<span class="proff">
+								Профессиональный ремонт стиральных машин Киеве, недорого, с
+								гарантией.
+							</span>
+						</div>
+					</div>
+					<div class="wrapper-nav">
+						<p class="schedule">Работаем Ежедневно: с 09:00 до 21:00</p>
+						<div class="wrapper-phone">
+							<span class="prompt">
+								*Нажмите на номер телефона для быстрого звонка
+							</span>
+							<a class="btn-phone" href="tel:+380979372223">
+								<img class="btn-img" src="img/phone-call.png" alt="телефон" />
+								<span>+38 (097) 937-22-23</span>
+							</a>
+							<a class="btn-phone" href="tel:+380507602223">
+								<img class="btn-img" src="img/phone-call.png" alt="телефон" />
+								<span>+38 (050) 760-22-23</span>
+							</a>
+
+						</div>
+					</div>
+				</div>
+				<div class="main_head">
+					<img
+							class="head-img2 wow bounceInRight"
+							src="img/machines.png"
+							alt="Машинка"
+					/>
+					<div class="wrapper-forma">
+						<h1 class="tagline-forma wow rubberBand animate__rubberBand">
+							Выездной ремонт стиральных машин
+							<span class="accent">в Киеве от 500грн</span>
+						</h1>
+
+						<form
+							enctype="multipart/form-data"
+							method="post"
+							onsubmit="send(event, 'send.php')"
+							id="form"
+							class="form-body wow bounceInLeft"
+						>
+							<div class="wrapper-contact">
+								<input
+									title="Укажите номер телефона в формате 0*********"
+									placeholder="Ваш номер телефона*"
+									id="formNumber"
+									name="number"
+									class="form-input _req"
+									type="tel"
+									maxlength="10"
+									minlength="10"
+									pattern="[0-9]+"
+								/>
+								<input
+									title="Имя может содержать только буквы"
+									pattern="[A-Za-zА-Яа-яЁёІіЇїЄє'’ʼ]{2,20}"
+									placeholder="Ваше имя*"
+									id="formName"
+									name="name"
+									class="form-input _req"
+									type="text"
+								/>
+							</div>
+							<textarea
+								placeholder="Опишите поломку в двух словах"
+								name="message"
+								id="forMessage"
+								cols="30"
+								class="form-input"
+								rows="10"
+							></textarea>
+							<button type="submit" class="btn-callback">
+								Оформить заявку
+							</button>
+						</form>
+					</div>
+					<img
+						class="head-img wow bounceInRight"
+						src="img/machines.png"
+						alt="Машинка"
+					/>
+				</div>
+			</div>
+		</header>
+		<section class="work-section">
+			<h1 class="wow rubberBand animate__rubberBand">Как мы работаем?</h1>
+
+			<div class="stages wow fadeInUp">
+				<div class="work-query">
+					<div class="wrapper-img">
+						<img src="img/work/site.png" alt="callback" />
+					</div>
+					<h3 class="work-title">
+						<span class="info">01. </span>ВЫ ЗВОНИТЕ ИЛИ ОСТАВЛЯЕТЕ ЗАЯВКУ
+					</h3>
+					<p class="work-text">
+						Опытный мастер связывается с Вами и отвечает на все вопросы - это
+						бесплатно.
+					</p>
+				</div>
+				<img class="arrow" src="img/work/1.png" alt="стрелка" />
+
+				<div class="work-query">
+					<div class="wrapper-img">
+						<img src="img/work/2.png" alt="callback" />
+					</div>
+					<h3 class="work-title">
+						<span class="info">02. </span>ОПРЕДЕЛЯЕМСЯ С ВЕРОЯТНОЙ ПОЛОМКОЙ
+					</h3>
+					<p class="work-text">
+						Выслушав Вас, мастер определяет вероятную поломку, озвучивает
+						стоимость работы.
+					</p>
+				</div>
+				<img class="arrow" src="img/work/1.png" alt="стрелка" />
+				<div class="work-query">
+					<div class="wrapper-img">
+						<img src="img/work/3.png" alt="callback" />
+					</div>
+
+					<h3 class="work-title"><span class="info">03. </span>ВЫЕЗД К ВАМ</h3>
+					<p class="work-text">
+						Вы договариваетесь про удобное время, когда мастер может к Вам
+						приехать и заняться ремонтом Вашем стиральной машины.
+					</p>
+				</div>
+				<img class="arrow" src="img/work/1.png" alt="стрелка" />
+
+				<div class="work-query">
+					<div class="wrapper-img">
+						<img src="img/logo.png" alt="callback" />
+					</div>
+
+					<h3 class="work-title"><span class="info">04. </span>ВСЕ РАБОТАЕТ</h3>
+					<p class="work-text">
+						Вместе с матером проверяете работу стиральной машины, если Вас все
+						устраивает - оплачиваете работу мастеру.
+					</p>
+				</div>
+			</div>
+		</section>
+		<section class="price-section">
+			<h1 class="wow rubberBand animate__rubberBand">
+				Поломки, которые мы устраним
+			</h1>
+			<div class="price-wrapper wow fadeInRight animate__fadeInRight">
+				<div class="item-price titel-price">
+					<p class="type">Виды работ</p>
+					<p class="price">Цена работы</p>
+				</div>
+				<div class="item-price">
+					<p class="type">Замена клапонов</p>
+					<p class="price">от 280грн</p>
+				</div>
+				<div class="item-price">
+					<p class="type">Замена водонагревательного элемента</p>
+					<p class="price">от 300грн</p>
+				</div>
+				<div class="item-price">
+					<p class="type">Замена манжеты люка</p>
+					<p class="price">от 250грн</p>
+				</div>
+				<div class="item-price">
+					<p class="type">Замена ручки люка</p>
+					<p class="price">от 250грн</p>
+				</div>
+				<div class="item-price">
+					<p class="type">Замена модуля управления</p>
+					<p class="price">от 600грн</p>
+				</div>
+				<div class="item-price">
+					<p class="type">Замена подшипникового узла</p>
+					<p class="price">от 650грн</p>
+				</div>
+				<div class="item-price">
+					<p class="type">Установка нового барабана</p>
+					<p class="price">от 800грн</p>
+				</div>
+				<div class="item-price">
+					<p class="type">Замена сальника</p>
+					<p class="price">от 450грн</p>
+				</div>
+				<div class="item-price">
+					<p class="type">Замена щеток на двигателе</p>
+					<p class="price">от 260грн</p>
+				</div>
+				<div class="item-price">
+					<p class="type">Установка новых амортизаторов</p>
+					<p class="price">от 320грн</p>
+				</div>
+				<div class="item-price">
+					<p class="type">Диагностика(в случае отказа от ремонта)</p>
+					<p class="price">от 100грн</p>
+				</div>
+				<div class="item-price">
+					<p class="type">Замена сливного насоса(помпы)</p>
+					<p class="price">от 350грн</p>
+				</div>
+			</div>
+		</section>
+		<section class="location-section">
+			<div class="container">
+				<h1 class="wow rubberBand animate__rubberBand">
+					Мы работаем по всему Киеву
+				</h1>
+				<div class="wrapper-location">
+					<div class="location-content wow bounceInLeft">
+						<h2>Почему нам доверяют ?</h2>
+						<p class="location-text">
+							МЫ занимаемся ремонтом бытовой техники с 2014 года. . К нам
+							регулярно обращаются, поскольку мы работаем на результат: Мы не
+							берем плату за выезд и диагностику в случае состоявшегося ремонта
+							стиральной машины. Стоимость Диагностики в случае отказа от
+							ремонта составляет 200грн. Указанные на сайте цены являются
+							ориентировочными и могут измениться как в меньшую, так и в большую
+							сторону. Общая стоимость ремонта стиральной машины зависит от
+							модели, степени неисправности, а так же, от стоимости необходимых
+							запчастей и расходных материалов. Основную часть комплектующих
+							наши мастера возят с собой, если знают какая модель машинки и
+							причину поломки. Наши мастера приедут к Вам уже в течении 60 минут
+							после звонка. Предлагаем только качественные оригинальные запчасти
+							рекомендованные производителем . Гарантия на ремонт - до 1 года.
+						</p>
+					</div>
+					<img
+						class="map wow bounceInRight"
+						src="img/location.gif"
+						alt="Локация"
+					/>
+				</div>
+				<div class="confidence-wrapper">
+					<div class="confidence-item">
+						<img src="img/confidence/1.png" alt="часы" />
+						<h3>Быстро</h3>
+						<p class="confridence-text">
+							Скоростные темпы ликвидации неисправности – то, чего нам удалось
+							достичь благодаря оперативному выезду мастера на дом, наличию всех
+							необходимых инструментов и комплектующих запчастей и материалов.
+						</p>
+					</div>
+					<div class="confidence-item animate__animated animate__wobble">
+						<img id="conf-img" src="img/confidence/22.png" alt="деньги" />
+						<h3>Бюджетно</h3>
+						<p class="confridence-text">
+							Ремонт стиральных машин недорого – кредо нашей компании. Мы
+							устанавливаем приемлемые расценки за счет того, что расходные
+							материалы приобретаем у производителей техники, а не у
+							посредников.
+						</p>
+					</div>
+					<div class="confidence-item">
+						<img src="img/confidence/3.png" alt="гарантия" />
+						<h3>Официальная гарантия</h3>
+						<p class="confridence-text">
+							На все виды работ вы обязательно получите гарантийный талон, если
+							обратились к нам. Мы не занимаемся халтурой, не оставляем клиентов
+							в беде с неисправностями техники.
+						</p>
+					</div>
+					<div class="confidence-item">
+						<img src="img/confidence/4.png" alt="дом" />
+						<h3>На дому</h3>
+						<p class="confridence-text">
+							Вы вызываете специалиста – он приезжает для проведения ремонта в
+							сжатые сроки. Это в значительной степени облегчает жизнь клиентам,
+							экономит их средства, так как теперь нет нужды искать автомобиль с
+							целью транспортировки в сервисный центр, туда и обратно.
+						</p>
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<section class="repairing-section">
+			<h1>Фирмы, которые мы ремонтируем</h1>
+			<div class="container">
+				<div class="item-brend wow flip">
+					<img src="img/brends/14.png" alt="beko" />
+				</div>
+				<div class="item-brend wow flip">
+					<img src="img/brends/aeg.png" alt="aeg" />
+				</div>
+				<div class="item-brend wow flip">
+					<img src="img/brends/ardo.png" alt="ardo" />
+				</div>
+				<div class="item-brend wow flip">
+					<img src="img/brends/ariston.png" alt="aristo" />
+				</div>
+				<div class="item-brend wow flip">
+					<img src="img/brends/bosh.png" alt="bosh" />
+				</div>
+				<div class="item-brend wow flip">
+					<img src="img/brends/electrolux.png" alt="electrolux" />
+				</div>
+				<div class="item-brend wow flip">
+					<img src="img/brends/gorenje.png" alt="gorenje" />
+				</div>
+				<div class="item-brend wow flip">
+					<img src="img/brends/indesit.png" alt="indesit" />
+				</div>
+				<div class="item-brend wow flip">
+					<img src="img/brends/lg.png" alt="lg" />
+				</div>
+				<div class="item-brend wow flip">
+					<img src="img/brends/samsung.png" alt="samsung" />
+				</div>
+				<div class="item-brend wow flip">
+					<img src="img/brends/sanussi.png" alt="sanussi" />
+				</div>
+				<div class="item-brend wow flip">
+					<img src="img/brends/simens.png" alt="simens" />
+				</div>
+				<div class="item-brend wow flip">
+					<img src="img/brends/whirlpool.png" alt="whirlpool" />
+				</div>
+				<div class="item-brend wow flip">
+					<img src="img/brends/2.png" alt="kiser" />
+				</div>
+			</div>
+			<p>И ещё 18 брендов</p>
+		</section>
+		<footer>
+			<div class="container">
+				<div class="wrapper-footer">
+					<img src="img/logo.png" alt="логотип" />
+					<h3>Срочный-Выездной Ремонт Стиральных машин в Киеве</h3>
+				</div>
+				<p class="schedule">Работаем Ежедневно: с 09:00 до 21:00</p>
+				<div class="wrapper-arrows">
+					<a class="arrow-up" href="#top"><img src="img/arrows.png" alt="вверх" /></a>
+					<span class="prompt">*Нажмите на стрелку для быстрого перехода вверх страницы</span>
+
+				</div>
+
+			</div>
+		</footer>
+		<script src="wow.min.js"></script>
+		<script src="main.js"></script>
+	</body>
+</html>
